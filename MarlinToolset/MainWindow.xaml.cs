5a3e73cf3bb8aaf16f99d2eb83fd2a49c2498d65
Nick Pateman <nickj@devoctomy.com>
2019-12-22 16:28:32 +0000

    Started code for configuring all printers.

--- MarlinToolset/MainWindow.xaml.cs
+++ MarlinToolset/MainWindow.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using MarlinToolset.Services;
-using MarlinToolset.Views;
 using System.Windows;
 
 namespace MarlinToolset
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private IPrinterConfigurationManagerService _printerConfigurationManagerService;
@@ -20,12 +16,7 @@ namespace MarlinToolset
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var printerConfigurationView = new PrinterConfigurationView();
-            var result = printerConfigurationView.ShowDialog();
-            if(result.HasValue)
-            {
-                _printerConfigurationManagerService.Add(printerConfigurationView.ViewModel.Model);
-            }
+            //test printers configuration view
         }
     }
 }
---
+++ MarlinToolset/ViewModels/PrintersConfigurationViewModel.cs
@@ -0,0 +1,59 @@
+﻿using MarlinToolset.Services;
+using MarlinToolset.Views;
+using ReactiveUI;
+using ReactiveUI.Validation.Abstractions;
+using ReactiveUI.Validation.Contexts;
+using ReactiveUI.Validation.Extensions;
+using System;
+using System.Reactive;
+
+namespace MarlinToolset.ViewModels
+{
+    public class PrintersConfigurationViewModel : ReactiveObject, IValidatableViewModel
+    {
+        public ValidationContext ValidationContext { get; }
+        public ReactiveCommand<Unit, Unit> Save { get; }
+        public Action OnSave { get; }
+        public ReactiveCommand<Unit, Unit> Add { get; }
+        public ReactiveCommand<Unit, Unit> Remove { get; }
+        public ReactiveCommand<Unit, Unit> Clear { get; }
+
+        private readonly IPrinterConfigurationManagerService _printerConfigurationManagerService;
+
+        public PrintersConfigurationViewModel(
+            IPrinterConfigurationManagerService printerConfigurationManagerService,
+            Action onSave)
+        {
+            _printerConfigurationManagerService = printerConfigurationManagerService;
+            OnSave = onSave;
+            ValidationContext = new ValidationContext();
+
+            // validation logic here
+
+            Add = ReactiveCommand.Create(new Action(OnAdd));
+            Remove = ReactiveCommand.Create(new Action(OnRemove));
+            Clear = ReactiveCommand.Create(new Action(OnClear));
+            Save = ReactiveCommand.Create(OnSave, this.IsValid());
+        }
+
+        private void OnAdd()
+        {
+            var printerConfigurationView = new PrinterConfigurationView();
+            var result = printerConfigurationView.ShowDialog();
+            if (result.HasValue)
+            {
+                _printerConfigurationManagerService.Add(printerConfigurationView.ViewModel.Model);
+            }
+        }
+
+        private void OnRemove()
+        {
+
+        }
+
+        private void OnClear()
+        {
+
+        }
+    }
+}
