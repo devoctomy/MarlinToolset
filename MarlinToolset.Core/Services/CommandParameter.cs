4c9ef5132c9551aec5cc139f2fba302bfe578c3d
Nick Pateman <nickj@devoctomy.com>
2019-12-28 17:12:32 +0000

    Started adding extensible command processors.

---
+++ MarlinToolset.Core/Services/CommandParameter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarlinToolset.Core.Services
+{
+    public class CommandParameter
+    {
+        public string Token { get; }
+        public string Value { get; set; }
+        public bool Optional { get; }
+        public string Description { get; }
+
+        public CommandParameter(
+            string token)
+        {
+            Token = token;
+        }
+
+        public CommandParameter(
+            string token,
+            bool optional)
+        {
+            Token = token;
+            Optional = optional;
+        }
+
+        public CommandParameter(
+            string token,
+            string description,
+            bool optional)
+        {
+            Token = token;
+            Description = description;
+            Optional = optional;
+        }
+
+        public CommandParameter(
+            string token,
+            string description,
+            string value,
+            bool optional)
+        {
+            Token = token;
+            Description = description;
+            Value = value;
+            Optional = optional;
+        }
+    }
+}
---
+++ MarlinToolset.Core/Services/ICommandProcessorService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MarlinToolset.Core.Services
+{
+    public interface ICommandProcessorService
+    {
+        string Command { get; }
+        string Description { get; }
+        string Url { get; }
+        IList<CommandParameter> Parameters { get; }
+
+        bool Process(PrinterCommand command);
+    }
+}
